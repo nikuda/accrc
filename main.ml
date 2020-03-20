7aebce78b3db45feeb4b16abb1a240ea8b34d306
Ante <nikuda@gmail.com>
2020-03-21 00:27:17 +1100

    Clean up

--- main.ml
+++ main.ml
@@ -1,47 +1,68 @@
 open Yojson.Basic.Util
 
-type session_type =
-  | Race
-  | Qualifying
-  | Practice
+module SessionType = struct
+  type t =
+    | Race
+    | Qualifying
+    | Practice
 
-let parse_session_type json =
-  let str = json |> member "sessionType" |> to_string in
-  match str with
-  | "R" -> Race
-  | "Q" -> Qualifying
-  | "P" -> Practice
-  | _ -> failwith "Unknown session type"
+  let parse json =
+    let str = json |> member "sessionType" |> to_string in
+    match str with
+    | "R" -> Race
+    | "Q" -> Qualifying
+    | "P" -> Practice
+    | _ -> failwith "Unknown session type"
 
-let session_type_to_string session_type =
-  match session_type with
-  | Race -> "Race"
-  | Qualifying -> "Qualifying"
-  | Practice -> "Practice"
-
-module Session : sig
-  type t = {
-    track_name : string;
-    session_type : session_type;
-  }
-  val create : session_type -> string -> t
-end = struct
-  type t = {
-    track_name : string;
-    session_type : session_type;
-  }
-  let create session_type track_name = { session_type; track_name }
+  let to_string session_type =
+    match session_type with
+    | Race -> "Race"
+    | Qualifying -> "Qualifying"
+    | Practice -> "Practice"
 end
 
-let parse_session json =
-  let track_name = json |> member "trackName" |> to_string in
-  let session_type = parse_session_type json in
-  Session.create session_type track_name
+module Track = struct
+  type name =
+    | BrandsHatch of int
+    | Monza of int
+
+  let parse json =
+    let str = json |> member "trackName" |> to_string in
+    match str with
+    | "brands_hatch" -> BrandsHatch 2018
+    | "brands_hatch2019" -> BrandsHatch 2019
+    | _ -> BrandsHatch 0
+
+  let to_string name =
+    let n, y =
+      match name with
+      | BrandsHatch year -> ("Brands Hatch", year)
+      | Monza year -> ("Monza", year)
+    in
+    n ^ " " ^ string_of_int y
+end
+
+module Session = struct
+  type t = {
+    index: int;
+    track_name : Track.name;
+    session_type : SessionType.t;
+  }
+
+  let create index session_type track_name = { index; session_type; track_name }
+
+  let parse json =
+    let index = json |> member "sessionIndex" |> to_int in
+    let track_name = Track.parse json in
+    let session_type = SessionType.parse json in
+    create index session_type track_name
+end
+
 
 let main () =
   let json = Yojson.Basic.from_channel stdin in
-  let r = parse_session json in
+  let r = Session.parse json in
   let open Session in
-  print_endline (session_type_to_string r.session_type)
+  (SessionType.to_string r.session_type) ^ " " ^ (string_of_int r.index) ^ " - " ^ (Track.to_string r.track_name) |> print_endline
 
 let () = main ()
