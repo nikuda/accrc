12e2ed3302db3455ca1bbaa0a53cd7b1471455d9
Ante <nikuda@gmail.com>
2020-04-05 00:32:58 +1100

    Read dir

--- src/main.ml
+++ src/main.ml
@@ -4,15 +4,14 @@ let show_time ms =
   let (m, s, ms) = (ms / 60000, (ms mod 60000) / 1000, ms mod 1000) in
   string_of_int m ^ ":" ^ string_of_int s ^ ":" ^ string_of_int ms
 
-let main () =
+let show_result r =
   let open Session in
-  let file_string = Files.open_file "./data/Round1_Suzuka_R1.json" in
-  let json = Yojson.Basic.from_string file_string in
-  let r = Session.parse json in
   print_string (Track.to_string r.track);
   print_string " - ";
   print_string ((SessionType.to_string r.session_type) ^ " " ^ (string_of_int r.index));
   print_string (if r.result.SessionResult.is_wet_session then " - WET" else "");
+  print_newline ();
+
   print_newline ();
   print_endline ("Best lap: " ^ (show_time r.result.SessionResult.best_lap));
   List.iteri (fun i t -> print_endline ("Best S" ^ string_of_int (i + 1) ^ ":  " ^ (show_time t))) r.result.SessionResult.best_splits;
@@ -21,5 +20,14 @@ let main () =
       c.Leaderboard.driver.Driver.short_name ^ " " ^ CupCategory.to_string c.Leaderboard.cup_category ^ " " ^
       string_of_int c.Leaderboard.car_number ^ "\t\t" ^
       Car.to_string c.Leaderboard.car_model)) r.result.SessionResult.leaderboard
+
+let parse_result file =
+  let file_string = Files.open_file (String.concat "" ["./data/"; file]) in
+  let json = Yojson.Basic.from_string file_string in
+  Session.parse json
+
+let main () =
+  let dir = Sys.readdir "./data" in
+  Array.iter (fun f -> show_result(parse_result f)) dir
 
 let () = main ()
