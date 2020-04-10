4b6e3adcae9ec66f09ed148ca4054cb33a056a58
Ante <nikuda@gmail.com>
2020-04-10 12:09:32 +1000

    FIx mktime issue

--- src/utils.ml
+++ src/utils.ml
@@ -1,11 +1,11 @@
 open Unix
 
-let epoch_seconds date =
+let epoch_seconds filename =
   let to_tm yy mm dd hour min ss =
     Unix.mktime {Unix.tm_sec=ss; tm_min=min; tm_hour=hour;
-                 tm_mday=dd; tm_mon=mm; tm_year=(2000+yy);
+                 tm_mday=dd; tm_mon=mm; tm_year=((2000 + yy) - 1900);
                  tm_wday=0; tm_yday=0; tm_isdst=false}
-  in Scanf.sscanf date "%02d%02d%02d_%02d%02d%02d" to_tm
+  in Scanf.sscanf filename "%02d%02d%02d_%02d%02d%02d" to_tm
 
 
 let string_of_tm tm =
