61a615bfa001aa83593e08c7ec569ac5268813d6
Ante <nikuda@gmail.com>
2020-04-13 17:31:10 +1000

    Use positional argument for read/watch

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,14 @@
 open Config
 open Cmdliner
 
+let info =
+  let doc = "race control" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <nikuda at gmail.com>." ]
+  in
+  Term.info "accrc" ~version:"%‌%VERSION%%" ~doc ~exits:Term.default_exits ~man
+
 let watch poll_interval =
   Printf.printf "Watching %s with %d sec interval\n" config.dir_path poll_interval;
   Files.watch config poll_interval
@@ -11,12 +19,20 @@ let watch_cmd =
 
 let watch_t = Term.(const watch $ watch_cmd)
 
-let info =
-  let doc = "race control" in
-  let man = [
-    `S Manpage.s_bugs;
-    `P "Email bug reports to <nikuda at gmail.com>." ]
-  in
-  Term.info "accrc" ~version:"%‌%VERSION%%" ~doc ~exits:Term.default_exits ~man
+let read =
+  let file_cache = Hashtbl.create 10000 in
+  Files.read_files config file_cache
 
-let () = Term.exit @@ Term.eval (watch_t, info)
+let main mode =
+  match mode with
+  | "read" -> read
+  | "watch" ->  watch 1
+  | _ -> print_endline "dunno"
+
+let mode =
+  let doc = "dingus" in
+  Arg.(value & pos 0 string "Revolt!" & info [] ~docv:"MSG" ~doc)
+
+let main_t = Term.(const main $ mode)
+
+let () = Term.exit @@ Term.eval (main_t, info)
