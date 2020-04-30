8cc5df1c6c1db3a92a6861547fd9af0b27d9f828
Ante <nikuda@gmail.com>
2020-04-30 20:53:17 +1000

    Add drivers table

--- src/data.ml
+++ src/data.ml
@@ -1,17 +1,16 @@
 open Sqlite3
 
 let create_table_sql =
-  "CREATE TABLE contacts (
-    contact_id INTEGER PRIMARY KEY,
+  "CREATE TABLE drivers (
+    id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email text NOT NULL UNIQUE,
-    phone text NOT NULL UNIQUE
+    short_name text NOT NULL,
+    player_id text NOT NULL UNIQUE
   );"
 
 let init () =
-  let mydb = db_open "test.db" in
+  let mydb = db_open "accrc.db" in
   match exec mydb create_table_sql with
   | Rc.OK -> print_endline "Ok"
   | r -> prerr_endline (Rc.to_string r); prerr_endline (errmsg mydb)
-
